09864607023a42068704708bcf9cc324e4302deb
Ashutosh Gupta <aashutosh.g27@gmail.com>
2021-03-15 05:20:32 +0530

    Delete PackagingAndDelivery directory

--- PackagingAndDelivery/src/main/java/com/returnordermanag/packageDeliveryModule/exception/ComponentTypeNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.returnordermanag.packageDeliveryModule.exception;
-
-public class ComponentTypeNotFoundException  extends RuntimeException{
-	private static final long serialVersionUID = 1L;
-
-	public ComponentTypeNotFoundException(String ComponentType) {
-		super("there is no no deliveryCharge with-"+ComponentType);
-	}
-}
